0eb6d9218591d314d7632305ed3f79f81521a0d5
Felipe <fmatheusog@gmail.com>
2020-06-18 23:52:42 -0300

    cleaned react file structure

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-      </header>
-    </div>
+    <h1>Hello Worrrrrld!</h1>
   );
 }
 
